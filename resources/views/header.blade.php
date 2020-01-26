72c7287974c7ab2bb9cf514415c0b34a14ce28f6
misakimeix <j3nnif3rsousa@gmail.com>
2020-01-26 09:30:27 -0300

    add laravel template

---
+++ resources/views/header.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ Asset('favicon.jpg') }}" type="image/x-icon"/>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark">
+  <nav class="navbar navbar-expand-lg text-ligth navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">AniFlix</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+      <div class=" collapse text-light navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/animes/home">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-ligth" href="/list">Lista de Animes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-ligth" href="/contato">Contato</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0" method="GET">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-dark  my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+  </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="/animes/a">a</a>
+    <a class="navbar-brand" href="/animes/b">b</a>
+    <a class="navbar-brand" href="/animes/c">c</a>
+    <a class="navbar-brand" href="/animes/d">d</a>
+    <a class="navbar-brand" href="/animes/e">e</a>
+    <a class="navbar-brand" href="/animes/f">f</a>
+    <a class="navbar-brand" href="/animes/g">g</a>
+    <a class="navbar-brand" href="/animes/h">h</a>
+    <a class="navbar-brand" href="/animes/i">i</a>
+    <a class="navbar-brand" href="/animes/j">j</a>
+    <a class="navbar-brand" href="/animes/k">k</a>
+    <a class="navbar-brand" href="/animes/l">l</a>
+    <a class="navbar-brand" href="/animes/m">m</a>
+    <a class="navbar-brand" href="/animes/n">n</a>
+    <a class="navbar-brand" href="/animes/o">o</a>
+    <a class="navbar-brand" href="/animes/p">p</a>
+    <a class="navbar-brand" href="/animes/q">q</a>
+    <a class="navbar-brand" href="/animes/r">r</a>
+    <a class="navbar-brand" href="/animes/s">s</a>
+    <a class="navbar-brand" href="/animes/t">t</a>
+    <a class="navbar-brand" href="/animes/u">u</a>
+    <a class="navbar-brand" href="/animes/v">v</a>
+    <a class="navbar-brand" href="/animes/w">w</a>
+    <a class="navbar-brand" href="/animes/x">x</a>
+    <a class="navbar-brand" href="/animes/y">y</a>
+    <a class="navbar-brand" href="/animes/z">z</a>
+
+  </div>
+  </nav>
